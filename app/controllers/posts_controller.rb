3e746ff3dc2f4e8e23b9eaa2cbe9c7e7abf6cbb9
kanghy22 <nasmurf1hk@gmail.com>
2018-02-15 20:41:36 -0500

    Assignment Rails: CRUD completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,13 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    #each_with_index method
+    @posts.each_with_index do |post, index|
+      #use modulus operator
+       if index % 5 == 0
+        post.title = "SPAM"
+       end
+    end
 
   end
 
