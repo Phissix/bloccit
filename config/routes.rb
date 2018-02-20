5cb4eb14db1216529014be05453832e45b88762d
kanghy22 <nasmurf1hk@gmail.com>
2018-02-20 18:36:43 -0500

    Assignment Rails: crudpt3 completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  get 'questions/index'
+
+  get 'questions/show'
+
+  get 'questions/new'
+
+  get 'questions/edit'
+
   # #1
+  resources :questions
+
   resources :posts
 
   get 'about' => 'welcome#about'
