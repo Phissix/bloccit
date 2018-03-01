4922f7bf0da2a634e926b1937ed4efc5e5b4b7fa
kanghy22 <nasmurf1hk@gmail.com>
2018-03-01 17:27:06 -0500

    Assignment Rails: auth completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -57,6 +57,14 @@ require 'random_data'
    password: 'helloworld'
  )
 
+ moderator = User.create!(
+   name:     'Moderator User',
+   email:    'moderator@example.com',
+   password: 'helloworld',
+   role:     'moderator'
+)
+
+
 
  puts "Seed finished"
  puts "#{User.count} users created"
