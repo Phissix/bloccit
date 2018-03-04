c457e7065b951a4c6fb7de3710e81b29c247ff87
kanghy22 <nasmurf1hk@gmail.com>
2018-03-04 17:38:22 -0500

    Assignment Rails: profiles completed

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,24 +4,24 @@ RSpec.describe Comment, type: :model do
   let(:topic) { create(:topic) }
   let(:user) { create(:user) }
   let(:post) { create(:post) }
-  let(:comment) { Comment.create!(body: 'Comment Body', post: post, user: user) }
+  let(:comment) { create(:comment) }
 
-     it { is_expected.to belong_to(:post) }
-     it { is_expected.to belong_to(:user) }
+  it { is_expected.to belong_to(:post) }
+  it { is_expected.to belong_to(:user) }
 
-     it { is_expected.to validate_presence_of(:body) }
-     it { is_expected.to validate_length_of(:body).is_at_least(5) }
+  it { is_expected.to validate_presence_of(:body) }
+  it { is_expected.to validate_length_of(:body).is_at_least(5) }
 
   describe "attributes" do
     it "has a body attribute" do
-      expect(comment).to have_attributes(body: "Comment Body")
+      expect(comment).to have_attributes(body: comment.body)
     end
   end
 
   describe "after_create" do
 # #22
     before do
-      @another_comment = Comment.new(body: 'Comment Body', post: post, user: user)
+      @another_comment = create(:comment, body: 'Comment Body', post: post, user: user)
     end
 
 # #23
